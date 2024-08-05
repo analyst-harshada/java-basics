5d1464dff191692e285e31d1817f570f50ff2de4
Harshada Javeri <harshada@Harshadas-MacBook-Air.local>
2024-08-05 22:40:56 +0530

    Loop example added

---
+++ loops.java
@@ -0,0 +1,23 @@
+public class loops {
+    public static void main(String[] args) {
+        // For Loop
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World");
+        }
+
+        // While Loop
+        int j = 0;
+        while (j < 5) {
+            System.out.println("Hello World");
+            j++;
+        }
+
+        // Do-While Loop
+        int k = 0;
+        do {
+            System.out.println("Hello World");
+            k++;
+        } while (k < 5);
+    }
+    
+}
